6f1b56584557bf8fe56dafb9ff1937d6a781984e
keeping <starunaway@gmail.com>
2021-03-26 10:24:07 +0800

    docs: ✏️ myPromise

---
+++ docs/JavaScript/Promise操作/myPromise.js
@@ -0,0 +1,65 @@
+class MyPromise {
+  constructor(fn) {
+    this.status = 'pending';
+    this.value = null;
+    this.handlers = {};
+    fn(this.resolve, this.reject);
+  }
+
+  resolve = (value) => {
+    if (this.status === 'pending') {
+      this.status = 'fulfilled';
+
+      this.value = value;
+      this.next(this.handlers);
+    }
+  };
+
+  reject = (err) => {
+    if (this.status === 'pending') {
+      this.status = 'rejected';
+      this.value = err;
+      this.next(this.handlers);
+    }
+  };
+
+  next = ({resolve, reject}) => {
+    switch (this.status) {
+      case 'fulfilled':
+        resolve && resolve(this.value);
+        break;
+      case 'rejected':
+        reject && reject(this.value);
+        break;
+      case 'pending':
+        this.handlers = {resolve, reject};
+    }
+  };
+
+  then = (success, fail) => {
+    return new MyPromise((nextResolve, nextReject) => {
+      this.next({
+        resolve: (value) => {
+          nextResolve(success(value));
+        },
+        reject: (err) => {
+          nextReject(fail(err));
+        },
+      });
+    });
+  };
+}
+
+let p = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(5);
+  });
+  console.log(666666);
+});
+
+// p.then((res) => {
+//   console.log(res);
+//   return 7;
+// }).then((res) => {
+//   console.log(res);
+// });
